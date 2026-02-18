67063fd07915a39580f2821fbfd918aec5cd265f
mstf74 <mostafa3333gg@gmail.com>
2026-02-18 21:13:05 +0200

    created operationResult for manual validation, added dosage form enum, started implementing inventoryService

---
+++ pharma/src/main/java/com/example/pharma/model/entity/catalog/DosageForm.java
@@ -0,0 +1,16 @@
+package com.example.pharma.model.entity.catalog;
+
+public enum DosageForm
+{
+    Tablet,
+    Capsule,
+    Syrup,
+    Suspension,
+    Injection,
+    Cream,
+    Ointment,
+    Inhaler,
+    Suppository,
+    Patch,
+    Drops
+}
