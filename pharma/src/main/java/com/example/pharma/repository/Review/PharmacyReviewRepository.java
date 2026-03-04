806000885f313a7cf6dc0f7e0a5b3f4b8f9963a1
mostafa-araheim <mostafa.araheim@gmail.com>
2026-03-04 02:53:22 +0200

    hotfix

--- pharma/src/main/java/com/example/pharma/repository/Review/PharmacyReviewRepository.java
+++ pharma/src/main/java/com/example/pharma/repository/Review/PharmacyReviewRepository.java
@@ -12,7 +12,7 @@ public interface PharmacyReviewRepository extends JpaRepository<PharmacyReview, 
     List<PharmacyReview> findByCustomer_UserId(Integer userId);
     List<PharmacyReview> findByPharmacy_PharmacyId(Integer pharmacyId);
 
-    @Query("SELECT new com.example.pharma.model.dto.ReviewDto(u.name, pr.rating, pr.comment) " +
+    @Query("SELECT new com.example.pharma.dto.review.ReviewDto(u.name, pr.rating, pr.comment) " +
             "FROM PharmacyReview pr " +
             "JOIN pr.customer c " +
             "JOIN c.user u " +
