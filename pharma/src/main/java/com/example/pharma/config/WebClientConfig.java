33ff9b66dbd5e9db70d745152ec27a1a31d4da39
mostafa-araheim <mostafa.araheim@gmail.com>
2026-06-12 00:25:17 +0300

    added api keys to yml file

--- pharma/src/main/java/com/example/pharma/config/WebClientConfig.java
+++ pharma/src/main/java/com/example/pharma/config/WebClientConfig.java
@@ -2,6 +2,7 @@ package com.example.pharma.config;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -9,13 +10,15 @@ import org.springframework.web.reactive.function.client.WebClient;
 @Configuration
 @RequiredArgsConstructor
 public class WebClientConfig {
+    @Value("${api.openai.key}")
+    private String openAiApiKey;
 
     @Bean
     @Qualifier("openAiWebClient")
     public WebClient openAiWebClient() {
         return WebClient.builder()
                 .baseUrl("https://api.freemodel.dev")
-                .defaultHeader("Authorization", "Bearer " + "fe_oa_1205c6850641a08b91cdf54caed416dfb5197cf5f3cb9a3f")
+                .defaultHeader("Authorization", "Bearer " + openAiApiKey)
                 .defaultHeader("Content-Type", "application/json")
                 .build();
     }
