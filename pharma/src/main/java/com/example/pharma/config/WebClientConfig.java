066f3a6bd6065940d0179ac35182fdde41f3f4e5
mostafa-araheim <mostafa.araheim@gmail.com>
2026-06-11 23:34:47 +0300

    included both models

--- pharma/src/main/java/com/example/pharma/config/WebClientConfig.java
+++ pharma/src/main/java/com/example/pharma/config/WebClientConfig.java
@@ -1,6 +1,7 @@
 package com.example.pharma.config;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -9,24 +10,21 @@ import org.springframework.web.reactive.function.client.WebClient;
 @RequiredArgsConstructor
 public class WebClientConfig {
 
-
-    private String openAiApiKey;
-//    @Bean
-//    public WebClient aiWebClient() {
-//
-//        HttpClient httpClient = HttpClient.create()
-//                .responseTimeout(Duration.ofSeconds(30));
-//
-//        return WebClient.builder()
-//                .baseUrl("https://dvpf7gmr-8000.euw.devtunnels.ms/")
-//                .clientConnector(new ReactorClientHttpConnector(httpClient))
-//                .build();
-//    }
     @Bean
-    public WebClient aiWebClient() {
+    @Qualifier("openAiWebClient")
+    public WebClient openAiWebClient() {
         return WebClient.builder()
                 .baseUrl("https://api.freemodel.dev")
                 .defaultHeader("Authorization", "Bearer " + "fe_oa_1205c6850641a08b91cdf54caed416dfb5197cf5f3cb9a3f")
+                .defaultHeader("Content-Type", "application/json")
+                .build();
+    }
+
+    @Bean
+    @Qualifier("geminiWebClient")
+    public WebClient geminiWebClient() {
+        return WebClient.builder()
+                .baseUrl("https://generativelanguage.googleapis.com")
                 .defaultHeader("Content-Type", "application/json")
                 .build();
     }
