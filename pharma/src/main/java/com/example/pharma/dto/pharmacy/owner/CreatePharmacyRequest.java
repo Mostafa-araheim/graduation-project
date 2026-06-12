10dba29d5ca7177b2d74fcd571a36a3f31bd3b8e
mostafa-araheim <mostafa.araheim@gmail.com>
2026-06-12 17:40:02 +0300

    fixed owner create pharmacy

--- pharma/src/main/java/com/example/pharma/dto/pharmacy/owner/CreatePharmacyRequest.java
+++ pharma/src/main/java/com/example/pharma/dto/pharmacy/owner/CreatePharmacyRequest.java
@@ -1,36 +1,38 @@
 package com.example.pharma.dto.pharmacy.owner;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.annotation.JsonProperty;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.time.LocalTime;
 
 public record CreatePharmacyRequest(
-        @JsonProperty("owner_user_id")
+
         Integer ownerUserId,
-        @JsonProperty("name")
+
         String name,
-        @JsonProperty("image_url")
-        String imageUrl,
+
         @JsonFormat(pattern = "HH:mm:ss")
         LocalTime openingTime,
+
         @JsonFormat(pattern = "HH:mm:ss")
         LocalTime closingTime,
-        @JsonProperty("is_24_hours")
+
         Boolean is24Hours,
-        @JsonProperty("street")
+
         String street,
-        @JsonProperty("city")
+
         String city,
-        @JsonProperty("postal_code")
+
         String postalCode,
-        @JsonProperty("country")
+
         String country,
-        @JsonProperty("apartment_number")
+
         String apartmentNumber,
-        @JsonProperty("latitude")
+
         Double latitude,
-        @JsonProperty("longitude")
-        Double longitude
+
+        Double longitude,
+
+        MultipartFile image
 ) {
 }
